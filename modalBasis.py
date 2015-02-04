8832b631aa06779748fbe80a31052868ece07f9a
Nazim Bharmal <n.abharmal@dur.ac.uk>
2015-02-04 07:33:28 +0000

    Cleaned up modalBasis test code to clarify its operation

--- modalBasis.py
+++ modalBasis.py
@@ -1,7 +1,10 @@
-from __future__ import print_function
-# What is this?
-# Generate modal bases
+"""General non-complete modal basis for arbitrary shaped apertures.
 
+For modal filtering, create modal functions which are limited (so non-complete) 
+which are not guaranteed to be either orthogonal or normalized.
+Currently based on Zernike-like forms and sinusoidal-like forms.
+"""
+from __future__ import print_function
 import gradientOperator
 import numpy
 
@@ -122,8 +125,9 @@ class FourierModalBasisType1(gradientOperator.geometryType1):
 # ------------------------------
 
 if __name__=="__main__":
-   # What is this?
-   # Test modal basis filtering and split modal reconstruction
+   print("What is this?")
+   print("Test modal basis filtering and split modal reconstruction")
+   print()
 
    import numpy
    import Zernike
@@ -132,34 +136,31 @@ if __name__=="__main__":
    import commonSeed
    import sys
    import kolmogorov
-
    import time
    numpy.random.seed(int(time.time()%1234))
 
-   baseSize=20
+   # -- configuration begins -----
    #
-   # naive means Tikhonov
-   # intermediate means 'intermediate layer restriction'
-   # laplacian means Laplacian approximation
-   # SVD means avoid direct inversion
-   regularizationType=['intermediate','laplacian','SVD','naive'][1]
-   modalPowers={'r':[1,2],'ang':[1]}
-
-
+   baseSize=20
+   mBidxs=[(1, 1, 1), (1, 1, 0), (1, 2, 1), (1, 3, 1), (1, 3, 0)]
    mask=Zernike.anyZernike(1,baseSize,baseSize/2,ongrid=1)\
          -Zernike.anyZernike(1,baseSize,baseSize/2/7.0,ongrid=1)
-   mask[baseSize/2]=0   # /
-   mask[:,baseSize/2]=0 # \ crude spider
+##
+##   mask[baseSize/2]=0   # /
+##   mask[:,baseSize/2]=0 # \ crude spider
+##
+#(redundant?)  modalPowers={'r':[1,2],'ang':[1]}
+   #
+   # -- configuration ends -------
+
+   
    mask=mask.astype(numpy.int32)
    nMask=int(mask.sum())
 
    gradOp=gradientOperator.gradientOperatorType1(pupilMask=mask)
    gradM=gradOp.returnOp()
 
-   thisModalBasis=modalBasis( mask,
-         [],[], orthonormalize=0, verbose=1 )
-   #mBs=modalBasis.orthomodalFunctions
-   mBidxs=[(1, 1, 1), (1, 1, 0), (1, 2, 1), (1, 3, 1), (1, 3, 0)]
+   thisModalBasis=modalBasis( mask, [],[], orthonormalize=0, verbose=1 )
    mBs=numpy.array([
       thisModalBasis.modalFunction( tmBidx[0],tmBidx[1],tmBidx[2] )
       for tmBidx in mBidxs ])
@@ -172,19 +173,17 @@ if __name__=="__main__":
    modalFilterM=mBs.T.dot(mBs)
 
    gradMplus=numpy.linalg.pinv(gradM,1e-6)
+   #
    gradmM=gradM.dot( modalFilterM )
    gradmMplus=numpy.linalg.pinv(gradmM,1e-6)
-   #gradzM=gradM.dot( numpy.identity(nMask)-modalFilterM )
+   #
    gradzM=gradM-gradmM
-   #gradzMplus=numpy.linalg.pinv(gradzM,1e-1)
    gradzMplus=gradMplus
    gradzMplus=(numpy.identity(nMask)-modalFilterM).dot(gradzMplus)
 
    print("Input data...",end="") ; sys.stdout.flush()
-
-   thisData=kolmogorov.TwoScreens(baseSize*2,
-               (nMask**0.5)/2.0,
-               flattening=2*numpy.pi/baseSize/2.0*30.)[0][:baseSize+2,:baseSize]
+   thisData=kolmogorov.TwoScreens( baseSize*2, (nMask**0.5)/2.0,
+           flattening=2*numpy.pi/baseSize/2.0*30.)[0][:baseSize+2,:baseSize]
    thisData=thisData[:-2] -thisData[2:] # represent shifted screen
    thisData-=thisData.mean()
    thisData/=(thisData.max()-thisData.min())
@@ -193,18 +192,15 @@ if __name__=="__main__":
    inputDataV=thisData.ravel()[gradOp.illuminatedCornersIdx] 
    # (m),inputDataV=numpy.array(
    # (m),   [ numpy.random.normal()*tb for tb in modalBasis.modalFunctions ]).sum(axis=0)
+   gradsV=numpy.dot( gradM, inputDataV ) # calculate input vector
+   print("(done)") ; sys.stdout.flush()
 
+
+   # prepare plots
    inputDataA=numpy.ma.masked_array(
       numpy.empty(gradOp.n_), gradOp.illuminatedCorners==0 )
    inputDataA.ravel()[gradOp.illuminatedCornersIdx]=inputDataV
-
-   print("(done)") ; sys.stdout.flush()
-
-   # calculate input vector
-   gradsV=numpy.dot( gradM, inputDataV )
-               
-   print("All data prepared") ; sys.stdout.flush()
-
+   #
    reconFilteredV=gradzMplus.dot(gradsV)
    reconModalV=gradmMplus.dot(gradsV)
    reconJointV=(gradzMplus+gradmMplus).dot(gradsV)
@@ -218,54 +214,29 @@ if __name__=="__main__":
    reconJointA.ravel()[gradOp.illuminatedCornersIdx]=reconJointV
    reconOrigA.ravel()[gradOp.illuminatedCornersIdx]=reconOrigV
 
-   pyp.subplot(4,3,1)
-   pyp.imshow(inputDataA)
-   pyp.colorbar()
-   pyp.title("i/p")
-   #
-   pyp.subplot(4,3,2)
-   pyp.imshow(reconFilteredA,vmax=inputDataA.max(),vmin=inputDataA.min())
-   pyp.title("recon,less Z")
-   #
-   pyp.subplot(4,3,3)
-   pyp.imshow(inputDataA-reconFilteredA,vmax=inputDataA.max()*0.1,vmin=inputDataA.min())
-   pyp.colorbar()
-   pyp.title("(delta)")
+   for subplot,toshow,thistitle in (
+         [ (4,3,1), inputDataA, "i/p" ],
+         [ (4,3,2), reconFilteredA, "recon, w/o Z" ],
+         [ (4,3,3), inputDataA-reconFilteredA, "(delta)" ],
+         [ (4,3,2+3), reconModalA, "recon, only Z" ],
+         [ (4,3,3+3), inputDataA-reconModalA, "(delta)" ],
+         [ (4,3,2+6), reconJointA, "recon, inc Z" ],
+         [ (4,3,3+6), inputDataA-reconJointA, "(delta)" ],
+         [ (4,3,2+9), reconOrigA, "recon, no Z" ],
+         [ (4,3,3+9), inputDataA-reconOrigA, "(delta)" ],
+         ):
+      pyp.subplot( subplot[0],subplot[1],subplot[2] )
+      pyp.imshow( toshow, vmax=inputDataA.max(),vmin=inputDataA.min() )
+      pyp.gca().xaxis.set_visible(0)
+      pyp.gca().yaxis.set_visible(0)
+      pyp.title(thistitle)
+      if subplot[-1]==1: pyp.colorbar()
 
-   pyp.title("i/p")
-   pyp.subplot(4,3,2+3)
-   pyp.imshow(reconModalA,vmax=inputDataA.max(),vmin=inputDataA.min())
-   pyp.title("recon,Z only")
-   pyp.subplot(4,3,3+3)
-   pyp.imshow(inputDataA-reconModalA,vmax=inputDataA.max(),vmin=inputDataA.min())
-   pyp.colorbar()
-   pyp.title("(delta)")
-
-   pyp.title("i/p")
-   pyp.subplot(4,3,2+6)
-   pyp.imshow(reconJointA,vmax=inputDataA.max(),vmin=inputDataA.min())
-   pyp.title("recon,joint")
-   pyp.subplot(4,3,3+6)
-   pyp.imshow(inputDataA-reconJointA,
-      vmax=inputDataA.max(),vmin=inputDataA.min())
-   pyp.colorbar()
-   pyp.title("(delta)")
-
-   pyp.title("i/p")
-   pyp.subplot(4,3,2+9)
-   pyp.imshow(reconOrigA,vmax=inputDataA.max(),vmin=inputDataA.min())
-   pyp.title("recon,orig")
-   pyp.subplot(4,3,3+9)
-   pyp.imshow(inputDataA-reconOrigA,
-      vmax=inputDataA.max(),vmin=inputDataA.min())
-   pyp.colorbar()
-   pyp.title("(delta)")
-
-   print("(input-recon_{{standard/original}}).var()/input.var()={0:5.3f}".format(
-     (inputDataV-reconOrigV).var()/inputDataV.var() ))
+   print("(input-recon_{{standard/original}}).var()/input.var()="+
+         "{0:5.3f}".format( (inputDataV-reconOrigV).var()/inputDataV.var() ))
    print("(input-recon_{{filtered}}).var()/input.var()={0:5.3f}".format(
-     (inputDataV-reconFilteredV).var()/inputDataV.var() ))
+        (inputDataV-reconFilteredV).var()/inputDataV.var() ))
    print("(input-recon_{{modal}}).var()/input.var()={0:5.3f}".format(
-     (inputDataV-reconModalV).var()/inputDataV.var() ))
+        (inputDataV-reconModalV).var()/inputDataV.var() ))
    print("(input-recon_{{joint}}).var()/input.var()={0:5.3f}".format(
-     (inputDataV-reconJointV).var()/inputDataV.var() ))
+        (inputDataV-reconJointV).var()/inputDataV.var() ))
