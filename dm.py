1943cf62ff73a48da633c06fd61f37844480530a
Nazim Ali Bharmal <nab@quinte.local>
2024-02-16 12:05:05 +0000

    More sophisticated transformations

--- dm.py
+++ dm.py
@@ -5,33 +5,69 @@ from __future__ import print_function
 import numpy
 
 
-class dm(object):
-   rotator=lambda self,x_y,ang : (
-      numpy.cos(ang/180.*numpy.pi)*(x_y[0]-self.npix[0]/2.)
-         -numpy.sin(ang/180.*numpy.pi)*(x_y[1]-self.npix[1]/2.)+self.npix[0]/2.,
-      numpy.sin(ang/180.*numpy.pi)*(x_y[0]-self.npix[0]/2.)
-         +numpy.cos(ang/180.*numpy.pi)*(x_y[1]-self.npix[1]/2.)+self.npix[1]/2. 
-         )
+def arrayFitter(size,ip):
+    '''Take the DM object, a surface representing the DM, and the size to fit
+    the surface onto.
+    Returns the fitted surface to the specified size.
+    '''
+	npix=numpy.array(ip).shape
+	assert len(npix)==2, "Only valid for 2D input"
+	assert len(size)==2, "Only valid for 2D size"
+    if tuple(npix)==tuple(size):
+        return ip
+    if npix[0]>size[0]:
+        ip=ip[npix[0]//2-size[0]//2:npix[0]//2-size[0]//2+size[0],:]
+    if npix[1]>size:
+        ip=ip[:,npix[1]//2-size[1]//2:npix[1]//2-size[1]//2+size[1]]
+    #
+    op = numpy.zeros( size, ip.dtype )
+    op[size[0]//2-ip.shape[0]//2:size[0]//2-ip.shape[0]//2+ip.shape[0],
+       size[1]//2-ip.shape[1]//2:size[1]//2-ip.shape[1]//2+ip.shape[1]]=ip
+    return( op ) 
 
-   def __init__(self, shape, actGeom, mask=None, rotation=0, within=0, ifScl=1,
-         lateralScl=[1,1], lateralOffset=[0,0]):
+class dm(object):
+   def __rotator__(self,x_y,ang,relLoc):
+      rx,ry=[ self.npix[i]/2.0-relLoc[i]*self.actspacing[i]
+            for i in (0,1) ]
+      nx=( numpy.cos(ang/180.*numpy.pi)*(x_y[0]-rx)
+          -numpy.sin(ang/180.*numpy.pi)*(x_y[1]-ry)+rx )
+      ny=( numpy.sin(ang/180.*numpy.pi)*(x_y[0]-rx)
+          +numpy.cos(ang/180.*numpy.pi)*(x_y[1]-ry)+ry )
+#      print(x_y,(nx,ny))
+      return( nx,ny )
+
+   def __init__(self, shape, actGeom, mask=None, rotation=0, rotationLoc=(0,0),
+            within=0, ifScl=1, lateralScl=[1,1], lateralOffset=[0,0]):
       '''npix (i) : is the sampling for the DM surface,
          actGeom (i,i) : represents the actuator geometry,
          mask [bool;array;2D] : the pupil mask, so can calculate which 
              actuators have a signifcant effect, in the sampling geometry,
-         rotation (f) : degrees,
-         within (b) : whether to let influence functions go over the edge,
-         ifScl (f) : scaling factor for the influence fn width,
-         lateralScl (f,f) : actuator spacing along axes relative to the
-            actuator grid, not the output axes,
-         lateralOffset (f,f) : is a relative factor of actuator spacing for
-            shifting the actuator grid.
+         rotation (f) : rotation [degrees, not radians],
+         rotationLoc (f,f) : rotation position (relative to NAG),
+         (within (b) : *REDUNDANT*
+               whether to let influence functions go over the edge,)0
+         ifScl (f) : scaling factor for the influence fn width (relative to
+               NAG),
+         lateralScl (f,f) : actuator spacing along axes (relative to NAG),
+         lateralOffset (f,f) : actuator displacement (relative to NAG),
+
+         There is a nominal actuator grid (NAG) which is based on the actuator
+         geometry spaced equally given the number of actuators and the number
+         of pixels specified, in each direction.  The positions of the
+         actuators are then assumed relative to the centre of the pixels and
+         transformed according to (in order),
+
+         (0. centre-aligned coordinates ;)
+         1. shift (lateralOffset) coordinates ;
+         2. scale (lateralScl) coordinates ;
+         3. rotate (rotation) coordinates, relative to offset (rotationLoc).
       '''
       self.npix=shape
       self.actGeom=actGeom
       self.nacts=actGeom[0]*actGeom[1]
-      self.rotation=rotation
-      self.within=within
+      self.rotation, self.rotationLoc=rotation, rotationLoc
+      self.within=within # now redundant
+      if self.within!=0: print("WARNING: within is a redundant option, will be removed")
       self.ifScl=ifScl
       self.mask=mask
       self.lateralScl=lateralScl
@@ -48,10 +84,8 @@ class dm(object):
    def coords(self):
       self.actCds=[]
       self.usable = numpy.ones(self.nacts) if self.mask is None else []
-#      offset=[ (
-#            self.actGeom[i]+(self.within!=0)-1)/2. for i in (0,1)]
-      self.actspacing=[ self.lateralScl[i]*self.npix[i]/float(
-               self.actGeom[i]+(self.within!=0)) for i in (0,1) ]
+      self.actspacing=[ self.lateralScl[i]*self.npix[i]/float(self.actGeom[i])
+            for i in (0,1) ]
       for i in range(self.nacts):
          (x,y)=(
             i//self.actGeom[1]-(self.actGeom[1]-1)/2.0-self.lateralOffset[1],
@@ -67,7 +101,7 @@ class dm(object):
             self.usable.append( self.mask[maskY,maskX] )
       if self.rotation!=0:
          for i in range(self.nacts):
-            self.actCds[i]=self.rotator(self.actCds[i],self.rotation)
+            self.actCds[i]=self.__rotator__(self.actCds[i],self.rotation,self.rotationLoc)
 
       self.usableIdx=numpy.flatnonzero(numpy.array(self.usable))
 
